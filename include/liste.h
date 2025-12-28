387e6c658d3a9ecc4efde2051300aeedad3c8740
maximelechine <maximelechine90@gmail.com>
2025-12-28 18:23:01 +0100

    Update liste.h

--- include/liste.h
+++ include/liste.h
@@ -1,12 +1,23 @@
 #ifndef LISTE_H
 #define LISTE_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
-typedef struct elem {
-	char *value;
-	struct elem* next;
-} listElement;
+typedef char* Element;
 
-typedef listElement* Liste;
+typedef struct Cellule {
+    Element valeur;
+    struct Cellule* suivant;
+} Cellule;
 
+typedef Cellule* Liste;
+Liste creerListe();
+bool appartient(Liste l, char* c);
+void afficherListe(Liste l);
+Liste ajouterQueue(Liste l, Element v);
+Liste supprimerElement(Liste l, Element v);
+void detruireListe(Liste l);
 
 #endif
